946ede30454808b8cdfcf466a469b0cb521db675
SadreevAmir <91326930+SadreevAmir@users.noreply.github.com>
2021-10-08 18:18:35 +0300

    много движущихся шариков, отскакивающих от стен

--- поймай шарик_1.py
+++ поймай шарик_1.py
@@ -3,9 +3,12 @@ from pygame.draw import *
 from random import randint
 pygame.init()
 
-FPS = 0.5
+FPS = 600
 points = 0
-screen = pygame.display.set_mode((1200, 900))
+screen_width = 1200
+screen_height = 900
+number_of_balls = 5
+screen = pygame.display.set_mode((screen_width, screen_height))
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -15,33 +18,46 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-
+x = []
+y = []
+vx = []
+vy = []
+r = []
+color = []
 def new_ball():
     '''рисует новый шарик '''
-    global x, y, r
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    global x, y, r, vy, vx
+    x.append(randint(100, 1100))
+    y.append(randint(100, 900))
+    r.append(randint(10, 100))
+    vx.append(randint(-1, 1))
+    vy.append(randint(-1, 1))
+    color.append(COLORS[randint(0, 5)])
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
+for i in range(number_of_balls):
+     new_ball()
 while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print('Click!')
-            if (event.pos[0] - x)**2 + (event.pos[1] - y)**2 <= r**2:
-                points = points + 1
-                print(points)
-                pygame.display.update()
-    new_ball()
-    pygame.display.update()
-    screen.fill(BLACK)
+    for i in range(number_of_balls):
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                finished = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if (event.pos[0] - x[i])**2 + (event.pos[1] - y[i])**2 <= r[i]**2:
+                points + points + 1
+                r[i] = 0
+        x[i] = x[i] + vx[i] 
+        y[i] = y[i] + vy[i]
+        if (x[i] < r[i]/2) or (x[i] > screen_width - r[i]/2):
+            vx[i] = -vx[i]
+        if (y[i] < r[i]/2) or (y[i] > screen_height - r[i]/2):
+            vy[i] = -vy[i]
+        circle(screen, color[i], (x[i], y[i]), r[i])
+
+        pygame.display.update()
+        screen.fill(BLACK)
 
 pygame.quit()
