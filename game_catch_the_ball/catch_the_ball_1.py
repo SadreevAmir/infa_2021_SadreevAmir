bbd6d553bf49b68c6d3a4d8913a771aa7794080f
Sadreev Amir <Sadreev.aa@phystech.edu>
2021-10-14 13:51:12 +0300

    квадрат со специфическим движением

--- game_catch_the_ball/catch_the_ball_1.py
+++ game_catch_the_ball/catch_the_ball_1.py
@@ -2,13 +2,16 @@ import pygame
 from pygame.draw import *
 from random import Random, random
 from random import randint
+import math
 pygame.init()
 
 points = 0
 screen_width = 1200
 screen_height = 900
-number_of_balls = 15
+number_of_balls = 10
+number_of_special_targets = 2
 difficulty = 1
+finished = False
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
@@ -20,24 +23,37 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-x = []
-y = []
-vx = []
-vy = []
-r = []
-color = []
+ball_x = []
+ball_y = []
+ball_vx = []
+ball_vy = []
+ball_r = []
+ball_color = []
+special_target_x = []
+special_target_y = []
+special_target_vx = []
+special_target_vy = []
+special_target_r = []
+special_target_color = []
 
 
 def new_ball():
     '''создает массивы с данными о каждом шарике'''
-    global x, y, r, vy, vx
-    x.append(randint(100, 1100))
-    y.append(randint(100, 900))
-    r.append(randint(10, 50))
-    vx.append(random()*2*difficulty - difficulty)
-    vy.append(random()*2*difficulty - difficulty)
-    color.append(COLORS[randint(0, 5)])
+    ball_x.append(randint(0, screen_width))
+    ball_y.append(randint(0, screen_height))
+    ball_r.append(randint(10, 50))
+    ball_vx.append(random()*2*difficulty - difficulty)
+    ball_vy.append(random()*2*difficulty - difficulty)
+    ball_color.append(COLORS[randint(0, 5)])
 
+def new_scpecial_target():
+    '''создает массив с данными специальных целей '''
+    special_target_x.append(randint(0, screen_width))
+    special_target_y.append(randint(0, screen_height))
+    special_target_r.append(randint(10, 50))
+    special_target_vy.append(random()*2*difficulty - difficulty)
+    special_target_vx.append(random()*2*difficulty - difficulty)
+    special_target_color.append(COLORS[randint(0, 5)])
 
 def scoreboard(points):
     '''отображает счет на табло'''
@@ -45,41 +61,78 @@ def scoreboard(points):
     text = font.render("Очки: "+str(points), True, RED)
     screen.blit(text, [screen_width/24, screen_height/24])
 
-finished = False
 for i in range(number_of_balls):
+    '''создание шариков'''
     new_ball()
 
+for i in range(number_of_special_targets):
+    '''создает специальные цели'''
+    new_scpecial_target()
+
 while not finished:
+    
     for i in range(number_of_balls):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if (event.pos[0] - x[i])**2 + (event.pos[1] - y[i])**2 <= r[i]**2:
+            if (event.pos[0] -ball_x[i])**2 + (event.pos[1] - ball_y[i])**2 <= ball_r[i]**2:
                 points = points + 1
                 print(points)
-                r[i] = 0
-        x[i] = x[i] + vx[i]
-        y[i] = y[i] + vy[i]
+                ball_r[i] = 0
+                
+        ball_x[i] = ball_x[i] + ball_vx[i]
+        ball_y[i] = ball_y[i] + ball_vy[i]
 
-        if x[i] < r[i]/2:
-            vx[i] = random()*difficulty
-            vy[i] = random()*2*difficulty - difficulty
+        if ball_x[i] < ball_r[i]/2:
+            ball_vx[i] = random()*difficulty
+            ball_vy[i] = random()*2*difficulty - difficulty
 
-        if x[i] > screen_width - r[i]/2:
-            vx[i] = -random()*difficulty
-            vy[i] = random()*2*difficulty - difficulty
+        if ball_x[i] > screen_width - ball_r[i]/2:
+            ball_vx[i] = -random()*difficulty
+            ball_vy[i] = random()*2*difficulty - difficulty
 
-        if y[i] > screen_height - r[i]/2:
-            vx[i] = random()*2*difficulty - difficulty
-            vy[i] = -random()*difficulty
+        if ball_y[i] > screen_height - ball_r[i]/2:
+            ball_vx[i] = random()*2*difficulty - difficulty
+            ball_vy[i] = -random()*difficulty
 
-        if y[i] < r[i]/2:
-            vx[i] = random()*2*difficulty - difficulty
-            vy[i] = random()*difficulty
+        if ball_y[i] < ball_r[i]/2:
+            ball_vx[i] = random()*2*difficulty - difficulty
+            ball_vy[i] = random()*difficulty
 
-        circle(screen, color[i], (x[i], y[i]), r[i])
+        circle(screen, ball_color[i], (ball_x[i], ball_y[i]), ball_r[i])
+    for i in range(number_of_special_targets):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            if abs(event.pos[0] - special_target_x[i]) <= special_target_r[i] or abs(event.pos[1] - special_target_y[i]) <= special_target_r[i]:
+                special_target_r[i] = 0
+                points = points + 2
+
+        if special_target_x[i] < special_target_r[i]/2:
+            special_target_vx[i] = random()*difficulty
+            special_target_vy[i] = random()*2*difficulty - difficulty
+
+        if special_target_x[i] > screen_width - special_target_r[i]/2:
+            special_target_vx[i] = -random()*difficulty
+            special_target_vy[i] = random()*2*difficulty - difficulty
+
+        if special_target_y[i] > screen_height - special_target_r[i]/2:
+            special_target_vx[i] = random()*2*difficulty - difficulty
+            special_target_vy[i] = -random()*difficulty
+
+        if special_target_y[i] < special_target_r[i]/2:
+            special_target_vx[i] = random()*2*difficulty - difficulty
+            special_target_vy[i] = random()*difficulty
+
+        special_target_x[i] = special_target_x[i] + special_target_vx[i] * (3- 3.8*abs(special_target_x[i] - screen_width/2)/screen_width)
+        special_target_y[i] = special_target_y[i] + special_target_vy[i] * (3 - 3.8*abs(special_target_y[i] - screen_height/2)/screen_height)
+
+        polygon(screen, special_target_color[i], 
+                        [(special_target_x[i] - special_target_r[i], special_target_y[i] - special_target_r[i]), 
+                        (special_target_x[i] + special_target_r[i], special_target_y[i] - special_target_r[i]),
+                        (special_target_x[i] + special_target_r[i], special_target_y[i] + special_target_r[i]),
+                        (special_target_x[i] - special_target_r[i], special_target_y[i] + special_target_r[i])])
         
     scoreboard(points)
     pygame.display.update()
