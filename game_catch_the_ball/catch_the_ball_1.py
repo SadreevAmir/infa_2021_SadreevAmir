4ca9d5f05223e100f938b90b8ccf6fd530922f80
Sadreev Amir <Sadreev.aa@phystech.edu>
2021-10-22 16:10:39 +0300

    программа запускается, шарики летят по баллистической траектории

--- game_catch_the_ball/catch_the_ball_1.py
+++ game_catch_the_ball/catch_the_ball_1.py
@@ -9,11 +9,11 @@ player_name = input()
 points = 0
 score = 0
 screen_width = 1200
-screen_height = 900
+screen_height = 800
 number_of_balls = 5
-number_of_special_targets = 5
+number_of_special_targets = 4
 turnes = 0
-difficulty = 1
+difficulty = 0.5
 finished = False
 screen = pygame.display.set_mode((screen_width, screen_height))
 
@@ -39,19 +39,27 @@ special_target_vy = []
 special_target_r = []
 special_target_color = []
 
-
 def new_ball():
-    '''создает массивы с данными о каждом шарике'''
+    '''создает массивы с данными о каждом шарике
+    ball_x, ball_y - координаты шарика
+    ball_r - радиус шарика
+    ball_vx, ball_vy - скорость шарика
+    ball_color - цвет шарика'''
     ball_x.append(randint(0, screen_width))
     ball_y.append(randint(0, screen_height))
-    ball_r.append(randint(10, 50))
+    ball_r.append(randint(5, 15))
     ball_vx.append(random()*2*difficulty - difficulty)
     ball_vy.append(random()*2*difficulty - difficulty)
     ball_color.append(COLORS[randint(0, 5)])
 
 
 def new_scpecial_target():
-    '''создает массив с данными специальных целей '''
+    '''создает массив с данными специальных целей
+    special_target_x, special_target_y - координаты цели
+    special_target_r - половина стороны квадрата цели
+    special_target_vx - скорость цели по оси x 
+    special_target_vy - скорость цели по оси y
+    '''
     special_target_x.append(randint(0, screen_width))
     special_target_y.append(randint(0, screen_height))
     special_target_r.append(randint(10, 50))
@@ -59,10 +67,19 @@ def new_scpecial_target():
     special_target_vx.append(random()*2*difficulty - difficulty)
     special_target_color.append(COLORS[randint(0, 5)])
 
+def draw_special_target(x: int, y: int, r: int, color: list):
+    '''x, y - координаты  центра специальной цели
+    r - радиус специальной цели
+    color - цвет специльной цели'''
+    polygon(screen, color,
+            [(x - r, y - r),(x + r, y - r),(x + r, y + r),(x - r, y + r)])
 
-def scoreboard(text, points, k):
+
+def scoreboard(text: str , points: int, k: int):
     '''отображает счет на табло
     k - отношение y координаты табло к высоте экрана
+    text - надпись на табло
+    points - значение, выводимое на табло
     '''
     font = pygame.font.Font(None, 25)
     text = font.render(text+str(points), True, RED)
@@ -70,11 +87,13 @@ def scoreboard(text, points, k):
 
 
 for i in range(number_of_balls):
-    '''создание шариков'''
+    '''создание шариков
+    number_of_balls - количество шариков на экране'''
     new_ball()
 
 for i in range(number_of_special_targets):
-    '''создает специальные цели'''
+    '''создает массивы с характеристиками специальныч целей
+    number_of_special targets - количесство специальных целей'''
     new_scpecial_target()
 
 while not finished:
@@ -96,8 +115,8 @@ while not finished:
         ball_x[i] = ball_x[i] + ball_vx[i]
         ball_y[i] = ball_y[i] + ball_vy[i]
 
-        '''проверка на выход за пределы экрана'''
-        if ball_x[i] < ball_r[i]:
+        '''проверка на столкноыение со стенами'''
+        if ball_x[i] < ball_r[i]/2:
             ball_vx[i] = random()*difficulty
             ball_vy[i] = random()*2*difficulty - difficulty
 
@@ -109,9 +128,12 @@ while not finished:
             ball_vx[i] = random()*2*difficulty - difficulty
             ball_vy[i] = -random()*difficulty
 
-        if ball_y[i] < ball_r[i]:
+        if ball_y[i] < ball_r[i]/2:
             ball_vx[i] = random()*2*difficulty - difficulty
             ball_vy[i] = random()*difficulty
+
+
+
 
         '''рисуется шарик'''
         circle(screen, ball_color[i], (ball_x[i], ball_y[i]), ball_r[i])
@@ -148,22 +170,23 @@ while not finished:
         special_target_y[i] = special_target_y[i] + special_target_vy[i] * \
             (3 - 5*abs(special_target_y[i] - screen_height/2)/screen_height)
 
-        '''рисование специальных целей'''
-        polygon(screen, special_target_color[i],
-                [(special_target_x[i] - special_target_r[i], special_target_y[i] - special_target_r[i]),
-                 (special_target_x[i] + special_target_r[i],
-                  special_target_y[i] - special_target_r[i]),
-                 (special_target_x[i] + special_target_r[i],
-                  special_target_y[i] + special_target_r[i]),
-                 (special_target_x[i] - special_target_r[i], special_target_y[i] + special_target_r[i])])
+        draw_special_target(special_target_x[i], special_target_y[i], special_target_r[i], special_target_color[i])
 
     scoreboard("Очки: ", points, 24)
+
     score = round(10000*points/(turnes + 1))
     scoreboard("Итоговый счет: ", score, 12)
     pygame.display.update()
     screen.fill(BLACK)
-    if turnes == 3000:
-        finished = True
+
+    if turnes == 1000:
+        for i in range(number_of_balls):
+            ball_r[i] = 0
+        for i in range(number_of_special_targets):
+            special_target_r[i] = 0
+            special_target_color = BLACK
+        scoreboard("Конец игры, итоговый счет: ", score, 6)
+
 with open("best_players.txt", 'a') as file:
         file.write(player_name + ":  " + str(score))
         file.write("\n")
